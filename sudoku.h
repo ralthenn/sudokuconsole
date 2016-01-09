f4550e51af0748562f6721d73105a4f507542c6a
ralthenn <neisayer@hotmail.com>
2016-01-08 23:00:07 -0500

    Update sudoku.h
    init and show commands

--- sudoku.h
+++ sudoku.h
@@ -3,8 +3,10 @@
 class Sudoku
 {
 public:
+  int init();
   int setgrid(int row, int col, int set);
   int getgrid(int row, int col);
+  int show();
 private:
   int grid[9][9][11]; // [0-8] = digits 1-9 suppositions, 9 = given, 10 = solved
 };
