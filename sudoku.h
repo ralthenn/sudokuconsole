dce5b0a295cae98ed135e3fef7a4e35f0575255b
ralthenn <neisayer@hotmail.com>
2016-01-09 21:45:44 -0500

    Update sudoku.h
    suppositions now work

--- sudoku.h
+++ sudoku.h
@@ -3,10 +3,19 @@
 class Sudoku
 {
 public:
+
+// basic commands
   int init();
+  int show();
+
+// grid manipulation
   int setgrid(int row, int col, int set);
   int getgrid(int row, int col);
-  int show();
+ 
+// checking functions
+  int check(int row, int col, int digit); 
+  int listsup(int row, int col);
+
 private:
   int grid[9][9][11]; // [0-8] = digits 1-9 suppositions, 9 = given, 10 = solved
 };
