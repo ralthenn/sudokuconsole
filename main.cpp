7300d67151ed72395f8a7ae453ddb3a81225c831
ralthenn <neisayer@hotmail.com>
2016-01-09 21:44:51 -0500

    Update main.cpp
    suppositions now work

--- main.cpp
+++ main.cpp
@@ -7,5 +7,18 @@ int main()
   su.init();
   std::cout << "Hello world!" << std::endl;  
   su.show();
+  std::cout << "Checking to see if a 5 would go in (5,5)..." << std::endl;
+  if (su.check(5,5,5))
+  {
+      std::cout << "Invalid supposition." << std::endl;
+  }
+  else
+  {
+      std::cout << "Valid supposition." << std::endl;
+  }
+  
+  std::cout << "Valid suppositions:" << std::endl;
+  su.listsup(5,5);
+  
   return 1;
 }
